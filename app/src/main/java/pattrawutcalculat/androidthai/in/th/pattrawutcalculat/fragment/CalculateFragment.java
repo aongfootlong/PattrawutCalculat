8574449f15c0b7aa44cc805ed3e1d8cee4525055
aongfootlong <Aong9791>
2018-01-09 15:44:10 +0700

    Back Controller

--- app/src/main/java/pattrawutcalculat/androidthai/in/th/pattrawutcalculat/fragment/CalculateFragment.java
+++ app/src/main/java/pattrawutcalculat/androidthai/in/th/pattrawutcalculat/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package pattrawutcalculat.androidthai.in.th.pattrawutcalculat.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import pattrawutcalculat.androidthai.in.th.pattrawutcalculat.MainActivity;
 import pattrawutcalculat.androidthai.in.th.pattrawutcalculat.R;
 
 /**
@@ -14,6 +16,41 @@ import pattrawutcalculat.androidthai.in.th.pattrawutcalculat.R;
  */
 
 public class CalculateFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//    Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+
+        ((MainActivity)getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getActivity().getSupportFragmentManager().popBackStack();
+
+            }
+        });
+
+    }
 
     @Nullable
     @Override
